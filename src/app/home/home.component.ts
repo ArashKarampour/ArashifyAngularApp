1049d5f410ac0a986392fb87eaeab7e8754f1093
Arash Karampour <karampour.arash@yahoo.com>
2025-06-09 17:37:56 +0200

    feat: adding paginator, productcomponent + changing logo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,21 +3,32 @@ import { ProductsService } from '../services/products.service';
 import { Prodcut, Products } from '../../types';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { ProductComponent } from "../components/product/product.component";
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    ProductComponent,
+    PaginatorModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
   products : Prodcut[] = [];
+  rows : number = 5;
+  totalRecords : number = 0;
   constructor(private prodcutsService: ProductsService){}
 
-  ngOnInit(): void{
+  onPageChange(event: any){
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number){
     this.prodcutsService.
-    getProducts("http://localhost:3000/clothes", {page:0,perPage:5}).
+    getProducts("http://localhost:3000/clothes", {page, perPage}).
     pipe(catchError(error => { // handling the erros here
       console.error(error);
       return throwError(()=> new Error("Something went wrong please try again later."))
@@ -25,11 +36,16 @@ export class HomeComponent implements OnInit {
     .subscribe({ //using the observable
       next: (products: Products) => { // using the returned value from observable
         this.products = products.items;
-        console.log(products);      
+        this.totalRecords = products.total;
+        // console.log(products);      
       },
       error: (err) => { // catching the returned error from error handler above
         console.error("Couldn't fetch the clothes data from the server!",err.message);
       },
     })
+  }
+
+  ngOnInit(): void{
+    this.fetchProducts(0, this.rows);
   }
 }
