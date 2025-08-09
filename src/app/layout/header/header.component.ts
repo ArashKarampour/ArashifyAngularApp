2bb2e01943d707f93f046574d1665ce651bd010f
Arash Karampour <karampour.arash@yahoo.com>
2025-08-09 12:55:11 +0200

    feat: adding header component and changing favicon

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, MenubarModule, CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  items = [
+    {
+      label: 'Home',
+      icon: 'pi pi-home',
+      route: '/'
+    },
+    {
+      label: 'About',
+      icon: 'pi pi-info-circle',
+      route: '/about'
+    },
+  ];
   toggleDarkMode() {
     const element : HTMLElement | null = document.querySelector('html');    
     (element as unknown as HTMLElement).classList.toggle('my-app-dark');
