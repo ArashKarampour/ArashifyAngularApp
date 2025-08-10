3f5fd18a0f4a4929ece2dd5f72a8184239334f02
Arash Karampour <karampour.arash@yahoo.com>
2025-08-10 20:02:53 +0200

    feat: changing form template-driven form to reactive forms with validations

--- src/app/components/product-popup/product-popup.component.ts
+++ src/app/components/product-popup/product-popup.component.ts
@@ -1,19 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject, OnChanges } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ValidatorFn, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
 import { Prodcut } from '../../../types';
 
 @Component({
   selector: 'app-product-popup',
   standalone: true,
-  imports: [DialogModule, ButtonModule, InputTextModule, FormsModule, RatingModule],
+  imports: [DialogModule, ButtonModule, InputTextModule, FormsModule, RatingModule, ReactiveFormsModule],
   templateUrl: './product-popup.component.html',
   styleUrl: './product-popup.component.scss'
 })
-export class ProductPopupComponent {
+export class ProductPopupComponent implements OnChanges {
   @Input() header: string = '';
 
   // Two-way-binding on visibilty with input and ouput
@@ -29,7 +29,28 @@ export class ProductPopupComponent {
   }; // to hold the product to be edited
   @Output() saveProduct: EventEmitter<Prodcut> = new EventEmitter<Prodcut>();
 
+  specialCharactherValidator() : ValidatorFn{
+    return (control: any) => {
+      const value = control.value;
+      if (value && /[!@#$%^&*(),.?":{}|<>]/.test(value)) {
+        return { specialCharacter: true }; // this means the value contains special characters which will lead to error
+      }
+      return null; // this means the value is valid
+    };
+  }
 
+  private formBuilder = inject(FormBuilder);
+  productForm = this.formBuilder.group({
+    id: [0], // id is not required for the form, but we keep it to send back to the parent component
+    name: ['', [Validators.required, this.specialCharactherValidator()]],
+    price: [''],
+    image: ['', [Validators.required]],
+    rating: [0]
+  });
+
+  ngOnChanges() {
+    this.productForm.patchValue(this.product);
+  }
 
   resetInitProduct(){
     this.product = {
@@ -48,12 +69,21 @@ export class ProductPopupComponent {
   }
 
   onSave(){
-    if(!this.product.name || !this.product.price || !this.product.image){
+    if(!this.productForm.value.name || !this.productForm.value.price || !this.productForm.value.image){
       alert("Please fill all the fields");
       return;
     }
-    this.saveProduct.emit(this.product);
-    this.resetInitProduct();
+    const {id,name, price, image, rating} = this.productForm.value;
+    this.saveProduct.emit({
+      id: id || 0,
+      name: name || '',
+      price: price || '',
+      image: image || '',
+      rating: rating || 0
+    }); // passing the value from the form to the parent component
+    console.log("Product saved:", this.productForm.value);
+    
+    // this.resetInitProduct();
     this.changeVisibilty();
   }
 
